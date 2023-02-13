5eb888819b66484e553fa3b78250fb097503aa7a
merndeveloperdhke <merndevelpler@gmail.comm>
2023-02-13 19:22:09 +0600

    commit2

--- index.js
+++ index.js
@@ -15,3 +15,26 @@ document.getElementById('btnSubmit').addEventListener('click',function(){
         alert("your email or password wrong.")
     }
 })
+
+var input = document.getElementById("user-password"); 
+input.addEventListener("keypress", function(event) { 
+  if (event.key === "Enter") { 
+    event.preventDefault(); 
+   
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    
+    // get password
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    console.log(password);
+
+    // verify email and password.
+    if(email === 'info@gmail.com' && password === 'secret'){
+        window.location.href='bank.html';
+    }else{
+        alert("your email or password wrong.")
+    }
+  
+  } 
+});
